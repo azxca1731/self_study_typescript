5d0238b23dac657ec879653a6ee8ef782481736c
azxca1731 <azxca1731@gmail.com>
2018-12-17 23:09:08 +0900

    function option

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Junghun",
+  age = 24,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, youa are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
