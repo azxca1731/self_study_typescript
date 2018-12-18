37656ee9eeaf98da384410a922baf9ce2c410f52
azxca1731 <azxca1731@gmail.com>
2018-12-18 23:48:10 +0900

    add class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Junghun",
-  age: 24,
-  gender: "male"
-};
+const Jung = new Human("Junghun", 24, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${
@@ -16,6 +17,6 @@ const sayHi = (person: Human): string => {
   }`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(Jung));
 
 export {};
