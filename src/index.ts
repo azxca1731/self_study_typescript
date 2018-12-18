7af06cc12a3efbdb4a3ea89ff46e8f72cc15b69f
azxca1731 <azxca1731@gmail.com>
2018-12-18 23:41:59 +0900

    add interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Junghun",
+  age: 24,
+  gender: "male"
 };
 
-console.log(sayHi("Junghun", 24, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }`;
+};
+
+console.log(sayHi(person));
 
 export {};
