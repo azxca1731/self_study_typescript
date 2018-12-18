e18492edd2846a3c674122f68965cb1040a5eacb
azxca1731 <azxca1731@gmail.com>
2018-12-18 23:30:41 +0900

    add tsc-watch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, youa are ${age}, you are a ${gender}`;
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
 
 console.log(sayHi("Junghun", 24, "male"));
